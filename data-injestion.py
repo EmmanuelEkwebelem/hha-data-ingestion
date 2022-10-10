eeaa60ab9b30e2c49e2eada32a3c40bec26df6fd
EmmanuelEkwebelem <93545665+EmmanuelEkwebelem@users.noreply.github.com>
2022-10-10 16:54:42 -0400

    Update

---
+++ data-injestion.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import requests 
+import json
+from google.cloud import bigquery 
+import db_dtypes
+
+### Section 1 ###
+tab1 = pd.read_excel('C:/Users/emman/Documents/GitHub/hha-data-ingestion/Data/Nigerian_Car_Prices.xlsx', 'Nigerian_Car_Prices')
+tab2 = pd.read_excel('C:/Users/emman/Documents/GitHub/hha-data-ingestion/Data/Nigerian_Car_Prices.xlsx', 'Nigerian_Car_Prices_Price')
+
+### Section 2 ###
+apiDataset = requests.get('https://data.cms.gov/data-api/v1/dataset/75e8dcb2-78eb-4a7d-a377-9108441966db/data') 
+apiDataset = apiDataset.json() 
+
+### Section 3 ###
+client = bigquery.Client.from_service_account_json('C:/Users/emman/Documents/GitHub/hha-data-ingestion/Data/hha-507-365120-eab88dc2636e.json')
+query_job = client.query("SELECT * FROM `bigquery-public-data.crypto_bitcoin_cash.transactions` LIMIT 100")
+results = query_job.result()
+bigquery1 = pd.DataFrame(results.to_dataframe())
+
+client = bigquery.Client.from_service_account_json('C:/Users/emman/Documents/GitHub/hha-data-ingestion/Data/hha-507-365120-eab88dc2636e.json')
+query_job = client.query("SELECT * FROM `bigquery-public-data.chicago_crime.crime` LIMIT 100")
+results = query_job.result()
+bigquery2 = pd.DataFrame(results.to_dataframe())
+
+print(bigquery1)
+print(bigquery2)
